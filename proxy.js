fd4f87553ff16776c53be9fd6b4b41820fe3e24c
Om Sharma <oms177794@gmail.com>
2026-05-09 07:40:14 +0530

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -6,12 +6,6 @@ export async function proxy(request) {
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
